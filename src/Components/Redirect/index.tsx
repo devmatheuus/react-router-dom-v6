d3441a8b3506c6ae189239a700409022e152d8f4
Matheus <matheus_lsilva@hotmail.com>
2023-02-24 22:29:21 -0300

    useNavigate

---
+++ src/Components/Redirect/index.tsx
@@ -0,0 +1,31 @@
+import './style.css';
+
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const Redirect = () => {
+  const [time, setTime] = useState(3);
+
+  const timeOut = useRef(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    timeOut.current = setTimeout(() => {
+      setTime((t) => t - 1);
+    }, 1000);
+
+    if (time <= 0) {
+      navigate('/');
+    }
+
+    return () => {
+      clearTimeout(timeOut.current);
+    };
+  }, [time]);
+
+  return (
+    <div>
+      <h1>Get out of here in {time}</h1>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { About } from './Components/About';
 import { Home } from './Components/Home';
 import { Menu } from './Components/Menu';
+import { NotFound } from './Components/NotFound';
 import { Post } from './Components/Post';
+import { Redirect } from './Components/Redirect';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -18,6 +20,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Route path="/about" element={<About />} />
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/posts" element={<Post />} />
+        <Route path="/redirect" element={<Redirect />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
